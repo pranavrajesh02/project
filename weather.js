bccfe87867be8b88d33824a4bd122f02b6f699bf
pranavrajesh02 <132791446+pranavrajesh02@users.noreply.github.com>
2023-12-29 11:22:11 -0500

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,55 @@
+//fetch data from a json file
+let weather = {
+  apiKey: "26f49622f0affc81a6eed68ce9a00d67",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("Enter a valid city name");
+          throw new Error("Enter a valid city name");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity, feels_like, temp_min, temp_max } = data.main;
+    const { speed } = data.wind;
+
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+    document.querySelector(".real-feel").innerText ="Real-Feel: " + feels_like + "°C"; // Corrected selector
+    document.querySelector(".temp_max").innerText = "Max temp: " + temp_max + "°C";
+    document.querySelector(".temp_min").innerText = "Min temp: " + temp_min + "°C";
+   
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    weather.search();
+  }
+});
+
+weather.fetchWeather("Columbus");
